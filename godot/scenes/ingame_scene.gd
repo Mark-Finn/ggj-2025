2c9f8be7713f52d9f6c13b12205741d307c3d1d8
cnRaun <cnrbase@gmail.com>
2025-01-21 20:33:10 -0600

    Finished Tetris Vanilla Build
    This one is just vanilla tetris before I reverse and try to add bubble tiles

--- godot/scenes/ingame_scene.gd
+++ godot/scenes/ingame_scene.gd
@@ -82,32 +82,46 @@ var active_layer : int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	new_game()
-	
+	$HUD.get_node("StartButton").pressed.connect(new_game)
+
 func new_game():
 	#reset variables
+	score = 0
 	speed = 1.0
+	game_running = true
 	steps = [0, 0, 0] #left, right, down
+	$HUD.get_node("GameOverLabel").hide()
+		#wipe everything
+	clear_piece()
+	clear_board()
+	clear_panel()
 	piece_type = pick_piece()
 	piece_atlas = Vector2i(shapes_full.find(piece_type), 0)
+	next_piece_type = pick_piece()
+	next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0)
 	create_piece()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		steps[0] += 10
-	elif Input.is_action_pressed("ui_right"):
-		steps[1] += 10
-	elif Input.is_action_pressed("ui_down"):
-		steps[2] += 10
-	
-	#apply downward movement each frame
-	steps[2] += speed
-	#move piece
-	for i in range(steps.size()):
-		if steps[i] > steps_req:
-			move_piece(directions[i])
-			steps[i] = 0
+	if game_running:
+		if Input.is_action_pressed("ui_left"):
+			steps[0] += 10
+		elif Input.is_action_pressed("ui_right"):
+			steps[1] += 10
+		elif Input.is_action_pressed("ui_down"):
+			steps[2] += 10
+		elif Input.is_action_pressed("ui_up"):
+			rotate_piece()
+
+
+		#apply downward movement each frame
+		steps[2] += speed
+		#move piece
+		for i in range(steps.size()):
+			if steps[i] > steps_req:
+				move_piece(directions[i])
+				steps[i] = 0
+
 
 func pick_piece():
 	var piece
@@ -120,6 +134,7 @@ func pick_piece():
 		piece = shapes.pop_front()
 	return piece
 
+
 func create_piece():
 
 	#reset variables
@@ -127,20 +142,45 @@ func create_piece():
 	cur_pos = start_pos
 	active_piece = piece_type[rotation_index]
 	draw_piece(active_piece, cur_pos, piece_atlas)
-	
+	#show next piece
+	draw_piece(next_piece_type[0], Vector2i(15, 6), next_piece_atlas)
+
+
 func clear_piece():
 	for i in active_piece:
 		erase_cell(active_layer, cur_pos + i)
 
+
 func draw_piece(piece, pos, atlas):
 	for i in piece:
 		set_cell(active_layer, pos + i, tile_id, atlas)
+
+
+func rotate_piece():
+	if can_rotate():
+		clear_piece()
+		rotation_index = (rotation_index + 1) % 4
+		active_piece = piece_type[rotation_index]
+		draw_piece(active_piece, cur_pos, piece_atlas)
+
 
 func move_piece(dir):
 	if can_move(dir):
 		clear_piece()
 		cur_pos += dir
 		draw_piece(active_piece, cur_pos, piece_atlas)
+	else:
+		if dir == Vector2i.DOWN:
+			land_piece()
+			check_rows()
+			piece_type = next_piece_type
+			piece_atlas = next_piece_atlas
+			next_piece_type = pick_piece()
+			next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0) #updating next piece color
+			clear_panel()
+			create_piece()
+			check_game_over()
+
 
 func can_move(dir):
 	#check for free space to move
@@ -150,5 +190,71 @@ func can_move(dir):
 			cm = false
 	return cm
 
+
+func can_rotate():
+	var cr = true
+	var temp_rotation_index = (rotation_index + 1) % 4
+	for i in piece_type[temp_rotation_index]:
+		if not is_free(i + cur_pos):
+			cr = false
+	return cr
+
+
 func is_free(pos):
 	return get_cell_source_id(board_layer, pos) == -1
+
+
+func land_piece():
+	#moves each bubble from the active layer to the board layer
+	for i in active_piece:
+		erase_cell(active_layer, cur_pos + i)
+		set_cell(board_layer, cur_pos + i, tile_id, piece_atlas)
+
+
+func clear_panel():
+	for i in range(14, 19):
+		for j in range(5, 9):
+			erase_cell(active_layer, Vector2i(i, j))
+
+# calls shift_rows() when a row is cleared
+func check_rows():
+	var row: int = ROWS
+	while row > 0:
+		var count = 0 
+		for i in range(COLS):
+			if not is_free(Vector2i(i + 1, row)):
+				count += 1
+		# erase row if full
+		if count == COLS:
+			shift_rows(row)
+			score += REWARD
+			$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score)
+			speed += ACCEL
+			print(speed)
+		else:
+			row -= 1
+
+
+func shift_rows(row):
+	var atlas
+	for i in range(row, 1, -1):
+		for j in range(COLS):
+			atlas = get_cell_atlas_coords(board_layer, Vector2i(j + 1, i - 1))
+			if atlas == Vector2i(-1, -1):
+				erase_cell(board_layer, Vector2i(j + 1, i))
+			else:
+				set_cell(board_layer, Vector2i(j + 1, i), tile_id, atlas)
+
+
+func clear_board():
+	for i in range(ROWS):
+		for j in range(COLS):
+			erase_cell(board_layer, Vector2i(j + 1, i + 1))
+
+
+func check_game_over():
+	for i in active_piece:
+		if not is_free(i + cur_pos):
+			land_piece()
+			$HUD.get_node("GameOverLabel").show()
+			game_running = false
