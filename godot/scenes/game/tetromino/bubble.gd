a0e210b629292835867b741974e0845d47cae74d
okdawg21 <acbennett21@gmail.com>
2025-01-24 20:53:58 -0600

    added animations and new art

--- godot/scenes/game/tetromino/bubble.gd
+++ godot/scenes/game/tetromino/bubble.gd
@@ -2,6 +2,8 @@ extends Node2D
 class_name Bubble
 
 
+
+
 func is_position_blocked(pos: Vector2) -> bool:
 	var area = get_node("DetectionRows/Row[%d]/Area[%d]" % [pos.y, pos.x])
 	var blocked = area is not Area2D || area.has_overlapping_areas()
@@ -17,3 +19,15 @@ func on_place() -> void:
 func distance_to_slam() -> float:
 	assert(%SlamCast.is_colliding())
 	return global_position.distance_to(%SlamCast.get_collision_point())
+	
+func animation_place() -> void:
+	%BubbleAnimationPlayer.speed_scale = 0.3
+	%BubbleAnimationPlayer.play("Place")
+
+func animation_idle() -> void:
+	%BubbleAnimationPlayer.speed_scale = 0.1
+	%BubbleAnimationPlayer.queue("idle")
+	
+func animation_coyote_input() -> void:
+	%BubbleAnimationPlayer.speed_scale = 1.0
+	%BubbleAnimationPlayer.play("coyote")
