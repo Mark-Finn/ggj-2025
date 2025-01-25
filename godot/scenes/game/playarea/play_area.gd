f2c09e0bf3c2b7b9bf5b6c4d7ba832ff8853494f
Boyer Simpkins <106032615+bwSimpkins@users.noreply.github.com>
2025-01-25 12:43:43 -0600

    Got basic functionality for calculating score

--- godot/scenes/game/playarea/play_area.gd
+++ godot/scenes/game/playarea/play_area.gd
@@ -4,12 +4,21 @@ extends Node2D
 const TETROMINO = preload("res://scenes/game/tetromino/tetromino.tscn")
 const PIXELS_PER_UNIT = 32
 const NUM_ROWS = 19
-var total_score
+var total_score = 0
+var bubbles_in_row: Array = []
 
 func _ready() -> void:
+	_populate_array()
 	_spawn_tetromino()
 	
+
+func _populate_array() -> void:
+	bubbles_in_row.resize(NUM_ROWS + 1)
+	for i in range(bubbles_in_row.size()):
+		bubbles_in_row[i] = 0
+		
 	
+
 func _spawn_tetromino() -> void:
 	var tetromino = TETROMINO.instantiate()
 	tetromino.letter = ["I", "O", "T", "J", "L", "S", "Z"].pick_random()
@@ -27,17 +36,12 @@ func _on_placed(bubbles: Array[Bubble], tetromino_position: Vector2) -> void:
 	
 	
 func get_score(bubbles: Array[Bubble]) -> void:
-	var bubbles_in_row: Array = []
-	bubbles_in_row.resize(NUM_ROWS)
-	for i in range(bubbles_in_row.size()):
-		bubbles_in_row[i] = 0
 	for bubble in bubbles:
 		var row = bubble.get_row()
-		print(row)
-		print(bubbles_in_row)
-		#bubbles_in_row[row] += 1
+		if row < 20:
+			bubbles_in_row[row] += 1
 	for row in bubbles_in_row:
 		if row == 10:
-			print("YOU SCORED", 100)
-		else:
-			print("NO SCORE")
+			total_score += 100
+			print(total_score)
+			bubbles_in_row[row] = 0
